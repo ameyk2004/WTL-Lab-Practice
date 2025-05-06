28ec44cf65f45394724f3ca05c2f3f820df2b542
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2025-05-06 14:02:24 +0530

    Form validation practice

---
+++ HTML_Practice/script.js
@@ -0,0 +1,16 @@
+function sayHello(){
+    return alert("Hello !")
+}
+
+function submit_form(){
+    console.log("Done !!!")
+    return alert("Submitted Successfuly")
+}
+
+function checkBlur(){
+    console.log("Not Allowed !!!")
+
+    
+
+    return alert("Field cannot be empty")
+}
